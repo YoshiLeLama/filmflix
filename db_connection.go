763384782b744e4854d141d5c8856163a0d718e0
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-01-06 19:51:27 +0100

    Initial commit

---
+++ db_connection.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
+)
+
+type film struct {
+	ID            primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Title         string             `json:"title"`
+	OriginalTitle string             `bson:"original_title" json:"original_title"`
+	Description   string             `json:"description"`
+	Director      string             `json:"director"`
+	Image         string             `json:"image"`
+	MovieBanner   string             `bson:"movie_banner" json:"movie_banner"`
+	ReleaseDate   string             `bson:"release_date" json:"release_date"`
+	Rating        string             `bson:"rt_score" json:"rt_score"`
+}
+
+func ConnectToDB() (client *mongo.Client) {
+	dbUrl := os.Getenv("DB_URL")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUrl))
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func GetCollection(client *mongo.Client, db string, coll string) *mongo.Collection {
+	return client.Database(db).Collection(coll)
+}
+
+func FindFilm(collection *mongo.Collection, criteria bson.D) film {
+	var result film
+	err := collection.FindOne(context.TODO(), criteria).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found\n")
+		return film{}
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+
+	return result
+}
+
+func FindFilms(collection *mongo.Collection, criteria bson.D) []film {
+	cursor, err := collection.Find(context.TODO(), criteria)
+	if err != nil {
+		panic(err)
+	}
+
+	var results []film
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+
+	return results
+}
+
+func AddFilm(collection *mongo.Collection, film film) interface{} {
+	film.ID = primitive.NewObjectID()
+
+	_, err := collection.InsertOne(context.TODO(), film)
+	if err != nil {
+		panic(err)
+	}
+
+	return film
+}
+
+func UpdateFilm(collection *mongo.Collection, idString string, data interface{}) int64 {
+	id, _ := primitive.ObjectIDFromHex(idString)
+
+	result, err := collection.UpdateOne(context.TODO(), bson.D{{"_id", id}}, bson.D{{"$set", data}})
+	if err != nil {
+		panic(err)
+	}
+
+	return result.ModifiedCount
+}
+
+func DeleteFilm(collection *mongo.Collection, idString string) int64 {
+	id, _ := primitive.ObjectIDFromHex(idString)
+
+	result, err := collection.DeleteOne(context.TODO(), bson.D{{"_id", id}})
+	if err != nil {
+		panic(err)
+	}
+
+	return result.DeletedCount
+}
+
+func DisconnectFromDB(client *mongo.Client) {
+	if err := client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+var client *mongo.Client
+var filmColl *mongo.Collection
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return
+	}
+
+	client = ConnectToDB()
+	filmColl = GetCollection(client, "films", "films")
+
+	defer DisconnectFromDB(client)
+
+	router := gin.Default()
+	router.StaticFile("/favicon.ico", "./app/favicon.ico")
+	router.Use(serveJS)
+	router.Static("/static", "./app/static")
+	router.GET("/", serveApp)
+
+	apiRoutes := router.Group("/api")
+	apiRoutes.GET("/films", getFilms)
+	apiRoutes.POST("/films", postFilms)
+	apiRoutes.PATCH("/films/:id", updateFilm)
+	apiRoutes.DELETE("/films/:id", deleteFilm)
+	apiRoutes.GET("/films/:id", getFilmByID)
+
+	err = router.Run(":" + os.Getenv("PORT"))
+	if err != nil {
+		return
+	}
+}
+
+func serveApp(c *gin.Context) {
+	c.File("./app/index.html")
+}
+func serveJS(c *gin.Context) {
+	if matched, _ := regexp.Match(`js\z`, []byte(c.Request.RequestURI)); matched {
+		c.Writer.Header().Set("Content-Type", "application/javascript")
+		c.Next()
+	}
+}
+
+func getFilms(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+
+	//c.IndentedJSON(http.StatusOK, films)
+	movies := FindFilms(filmColl, bson.D{})
+
+	c.IndentedJSON(http.StatusOK, movies)
+}
+
+func postFilms(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+
+	var newFilm film
+
+	if err := c.BindJSON(&newFilm); err != nil {
+		return
+	}
+
+	result := AddFilm(filmColl, newFilm)
+	c.IndentedJSON(http.StatusCreated, result)
+}
+
+func updateFilm(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+
+	var updateData interface{}
+
+	if err := c.BindJSON(&updateData); err != nil {
+		return
+	}
+
+	result := UpdateFilm(filmColl, c.Param("id"), updateData)
+	c.IndentedJSON(http.StatusNoContent, result)
+}
+
+func deleteFilm(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+
+	result := DeleteFilm(filmColl, c.Param("id"))
+	c.IndentedJSON(http.StatusNoContent, result)
+}
+
+func getFilmByID(c *gin.Context) {
+	id, _ := primitive.ObjectIDFromHex(c.Param("id"))
+
+	film := FindFilm(filmColl, bson.D{{"_id", id}})
+
+	if film.Title != "" {
+		c.IndentedJSON(http.StatusOK, film)
+		return
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "film not found"})
+}
